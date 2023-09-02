23985c0190b7889c0917a6ac1bbe60cc09e9b006
Nexadis <37233902+Nexadis@users.noreply.github.com>
2023-09-02 11:12:24 +0300

    FanOut pattern

--- main.go
+++ main.go
@@ -38,11 +38,30 @@ func FanIn(sources ...<-chan int) <-chan int {
 	return dest
 }
 
+func FanOut(source <-chan int, n int) []<-chan int {
+	destinations := make([]<-chan int, 0, n)
+	for i := 0; i < n; i++ {
+		i := i
+		fmt.Printf("Job %d is run\n", i)
+		dest := make(chan int)
+		destinations = append(destinations, dest)
+		go func() {
+			defer close(dest)
+			for val := range source {
+				dest <- val
+			}
+			fmt.Printf("Job %d is done\n", i)
+		}()
+	}
+	return destinations
+}
+
 func main() {
 	channels := 5
 	dataLen := 3
 	sleepTime := 500 * time.Millisecond
 	sources := make([]<-chan int, 0, channels)
+	border("[ Fan in ]")
 
 	for i := 0; i < channels; i++ {
 		ch := make(chan int)
@@ -63,4 +82,42 @@ func main() {
 	for d := range dest {
 		fmt.Println(d)
 	}
+	border("[ Fan out ]")
+
+	source := make(chan int)
+	dests := FanOut(source, channels)
+	go func() {
+		for i := 1; i <= dataLen*channels; i++ {
+			source <- i
+		}
+		close(source)
+	}()
+
+	var wg sync.WaitGroup
+	wg.Add(len(dests))
+
+	for i, ch := range dests {
+		go func(i int, d <-chan int) {
+			defer wg.Done()
+
+			for val := range d {
+				fmt.Printf("[%d] got %d\n", i, val)
+			}
+		}(i, ch)
+	}
+	wg.Wait()
+}
+
+func border(msg string) {
+	for i := 0; i < 80; i++ {
+		fmt.Print("-")
+	}
+	fmt.Println()
+
+	fmt.Println(msg)
+
+	for i := 0; i < 80; i++ {
+		fmt.Print("-")
+	}
+	fmt.Println()
 }
